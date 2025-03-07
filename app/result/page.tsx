a5d5d34287ec8c63f08188ca002a9cb702c1731d
myungsangBaek <bms6168@naver.com>
2025-03-07 14:36:39 +0900

    fix: 빌드에러 수정

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -2,7 +2,37 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+
 export default function Result() {
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto p-6 sm:p-8">
+      <div className="bg-white rounded-3xl shadow-[0_20px_60px_-15px_rgba(0,0,0,0.1)] p-8">
+        <div className="space-y-6">
+          {/* 이미지 */}
+          <div className="flex justify-center">
+            <Image src="/couple2.png" alt="MBTI" width={100} height={100} />
+          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <ResultContent />
+          </Suspense>
+          <div className="pt-4">
+            <Link
+              href="/"
+              className="block w-full bg-[#F3E2D3] hover:bg-[#ebd3c0] text-[#584848] font-bold py-4 px-6 rounded-2xl 
+                transition-all duration-200 ease-in-out transform hover:scale-[1.02] hover:shadow-lg
+                text-center"
+            >
+              메인으로 돌아가기
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ResultContent() {
   const searchParams = useSearchParams();
   const result = searchParams.get('result');
 
@@ -23,27 +53,5 @@ export default function Result() {
     </p>
   ));
 
-  return (
-    <div className="min-h-screen max-w-2xl mx-auto p-6 sm:p-8">
-      <div className="bg-white rounded-3xl shadow-[0_20px_60px_-15px_rgba(0,0,0,0.1)] p-8">
-        <div className="space-y-6">
-          {/* 이미지 */}
-          <div className="flex justify-center">
-            <Image src="/couple2.png" alt="MBTI" width={100} height={100} />
-          </div>
-          <div className="space-y-2">{formattedResult}</div>
-          <div className="pt-4">
-            <Link
-              href="/"
-              className="block w-full bg-[#F3E2D3] hover:bg-[#ebd3c0] text-[#584848] font-bold py-4 px-6 rounded-2xl 
-                transition-all duration-200 ease-in-out transform hover:scale-[1.02] hover:shadow-lg
-                text-center"
-            >
-              메인으로 돌아가기
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="space-y-2">{formattedResult}</div>;
 }
